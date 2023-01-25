fcbd3d134c6731c09d7a758a0dc99d1901e68861
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-01-25 13:09:26 -0300

    add iterator header

---
+++ iterator.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 10:24:50 by maolivei          #+#    #+#             */
+/*   Updated: 2023/01/25 13:04:49 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ITERATOR_HPP
+#define FT_ITERATOR_HPP
+
+#include "iterator/iterator_traits.hpp"
+#include "iterator/random_access_iterator.hpp"
+#include "iterator/reverse_iterator.hpp"
+
+#endif /* FT_ITERATOR_HPP */
