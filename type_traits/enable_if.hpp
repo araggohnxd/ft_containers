45848ec5ecc2a8ba528faf58d33dc8b3c1ee6f5d
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-01-25 21:53:46 -0300

    enable_if defaults T to void

--- type_traits/enable_if.hpp
+++ type_traits/enable_if.hpp
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 10:24:50 by maolivei          #+#    #+#             */
-/*   Updated: 2023/01/25 12:57:52 by maolivei         ###   ########.fr       */
+/*   Updated: 2023/01/25 21:53:24 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 namespace ft {
 
-template <bool, typename>
+template <bool B, typename T = void>
 struct enable_if {};
 
 template <typename T>
