208269b3c97c553b37ce86c2237cbbd48c7156e7
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-01-26 21:27:48 -0300

    add iterator functions (distance)

--- iterator.hpp
+++ iterator.hpp
@@ -6,13 +6,14 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 10:24:50 by maolivei          #+#    #+#             */
-/*   Updated: 2023/01/25 13:04:49 by maolivei         ###   ########.fr       */
+/*   Updated: 2023/01/26 16:37:45 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ITERATOR_HPP
 #define FT_ITERATOR_HPP
 
+#include "iterator/iterator_functions.hpp"
 #include "iterator/iterator_traits.hpp"
 #include "iterator/random_access_iterator.hpp"
 #include "iterator/reverse_iterator.hpp"
---
+++ iterator/iterator_functions.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_functions.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 20:56:09 by maolivei          #+#    #+#             */
+/*   Updated: 2023/01/26 18:33:32 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ITERATOR_FUNCTIONS_HPP
+#define FT_ITERATOR_FUNCTIONS_HPP
+
+#include "iterator_traits.hpp"
+
+namespace ft {
+
+template <typename Iter>
+typename ft::iterator_traits<Iter>::difference_type distance(Iter first, Iter last)
+{
+    typename ft::iterator_traits<Iter>::difference_type _distance = 0;
+
+    while (first++ != last)
+        ++_distance;
+    return (_distance);
+}
+
+} /* namespace ft */
+
+#endif /* FT_ITERATOR_FUNCTIONS_HPP */
