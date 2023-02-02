4cfe8ff5784db746f8497e82aefdd71d3d3d7515
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-02-02 20:47:23 -0300

    add vector constructors testing

---
+++ tests/test_utils.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_utils.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 19:25:03 by maolivei          #+#    #+#             */
+/*   Updated: 2023/02/02 20:24:15 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_UTILS_HPP
+#define TEST_UTILS_HPP
+
+#include "doctest.h"
+#include "vector.hpp"
+#include <vector>
+
+#endif /* TEST_UTILS_HPP */
