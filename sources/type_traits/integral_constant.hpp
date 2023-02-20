31f8fe2ee6300daf89cf305e051b4828ee71288a
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-02-20 12:15:08 -0300

    parenthesis

--- sources/type_traits/integral_constant.hpp
+++ sources/type_traits/integral_constant.hpp
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 10:24:50 by maolivei          #+#    #+#             */
-/*   Updated: 2023/01/31 19:34:04 by maolivei         ###   ########.fr       */
+/*   Updated: 2023/02/20 11:21:11 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ struct integral_constant {
         typedef T                       value_type;
         typedef integral_constant<T, v> type;
 
-        operator value_type() { return value; }
+        operator value_type() { return (value); }
 };
 
 typedef integral_constant<bool, true>  true_type;
